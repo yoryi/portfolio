f3bffb8d0d58e99100af398ac870ca5f9730241b
yoryi <yoryiojeda@outlook.com>
2023-03-08 19:54:01 -0500

    chore: removed location

--- components/About/AboutCard.js
+++ components/About/AboutCard.js
@@ -8,8 +8,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple"> Jorgihno Ojeda </span>
-            from <span className="purple"> Barranquilla, Colombia.</span>
+            Hi Everyone, I am <span className="purple"> Jorgihno Ojeda </span>.
             <br />
             <br />
             I am a Full-Stack React/React Native/Next.js/Node.js/AWS/Firebase
